ee2200d8d9d87aedc8e15d639990796e68cf169d
c7harry <76096139+c7harry@users.noreply.github.com>
2025-06-03 18:13:41 -0700

    clear index file to see exact timeframe of objects

--- script.js
+++ script.js
@@ -1,55 +1,44 @@
-const rectObj = {
+const rectObjOriginal = {
     name: 'Rectangle 1',
     width: 10,
     height: 10,
 };
+const rectObj = { ...rectObjOriginal };
 
-// Seal the rectObj
 Object.seal(rectObj);
 
-// Freeze the circleObj
-Object.freeze(rectObj);
+const rectDescriptors = Object.getOwnPropertyDescriptors(rectObj);
 
-let descriptors = Object.getOwnPropertyDescriptors(rectObj);
-
-// Modify rectObj
 rectObj.color = 'red';
 delete rectObj.name;
 rectObj.width = 20;
 
-const circleObj = {
+const circleObjOriginal = {
     name: 'Circle 1',
     radius: 30,
 };
+const circleObj = { ...circleObjOriginal };
 
-// Freeze the circleObj
 Object.freeze(circleObj);
 
-// Modify circleObj
-descriptors = Object.getOwnPropertyDescriptors(circleObj);
+const circleDescriptors = Object.getOwnPropertyDescriptors(circleObj);
+
 circleObj.color = 'blue';
 delete circleObj.name;
 circleObj.name = 'New Name';
 
-// Display Results for rectObj
-document.getElementById("rectObjOriginal").innerText = JSON.stringify({
-    name: 'Rectangle 1',
-    width: 10,
-    height: 10
-});
+window.onload = function() {
+    // Rectangle
+    document.getElementById("rectObjOriginal").innerText = JSON.stringify(rectObjOriginal, null, 2);
+    document.getElementById("rectObjModified").innerText = JSON.stringify(rectObj, null, 2);
+    document.getElementById("rectDescriptors").innerText = JSON.stringify(rectDescriptors, null, 2);
+    document.getElementById("isRectObjSealed").innerText = Object.isSealed(rectObj) ? "Yes" : "No";
+    document.getElementById("isRectObjFrozen").innerText = Object.isFrozen(rectObj) ? "Yes" : "No";
 
-document.getElementById("rectObjModified").innerText = JSON.stringify(rectObj);
-document.getElementById("isRectObjSealed").innerText = Object.isSealed(rectObj) ? "Yes" : "No";
-document.getElementById("isRectObjFrozen").innerText = Object.isFrozen(rectObj) ? "Yes" : "No";
-
-// Display Results for circleObj
-document.getElementById("circleObjOriginal").innerText = JSON.stringify({
-    name: 'Circle 1',
-    radius: 30
-});
-
-document.getElementById("circleObjModified").innerText = JSON.stringify(circleObj);
-document.getElementById("isCircleObjSealed").innerText = Object.isSealed(circleObj) ? "Yes" : "No";
-document.getElementById("isCircleObjFrozen").innerText = Object.isFrozen(circleObj) ? "Yes" : "No";
-
-document.getElementById("descriptors").innerText = JSON.stringify(descriptors, null, 2);
+    // Circle
+    document.getElementById("circleObjOriginal").innerText = JSON.stringify(circleObjOriginal, null, 2);
+    document.getElementById("circleObjModified").innerText = JSON.stringify(circleObj, null, 2);
+    document.getElementById("circleDescriptors").innerText = JSON.stringify(circleDescriptors, null, 2);
+    document.getElementById("isCircleObjSealed").innerText = Object.isSealed(circleObj) ? "Yes" : "No";
+    document.getElementById("isCircleObjFrozen").innerText = Object.isFrozen(circleObj) ? "Yes" : "No";
+};
